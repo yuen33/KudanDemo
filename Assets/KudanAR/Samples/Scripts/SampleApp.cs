3e572604572ab6303b41df4e1a60c8341e0af4fc
yuen33 <yuen33@163.com>
2016-04-12 11:19:34 +0800

    v0

---
+++ Assets/KudanAR/Samples/Scripts/SampleApp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kudan.AR.Samples
+{
+	public class SampleApp : MonoBehaviour
+	{
+        public KudanTracker _kudanTracker;
+        public TrackingMethodMarker _markerTracking;
+        public TrackingMethodMarkerless _markerlessTracking;
+
+        public void MarkerClicked()
+        {
+            _kudanTracker.ChangeTrackingMethod(_markerTracking);
+        }
+
+        public void MarkerlessClicked()
+        {
+            _kudanTracker.ChangeTrackingMethod(_markerlessTracking);
+        }
+
+        public void StartClicked()
+        {
+            // from the floor placer.
+            Vector3 floorPosition;
+            Quaternion floorOrientation;
+
+            _kudanTracker.FloorPlaceGetPose(out floorPosition, out floorOrientation);
+            _kudanTracker.ArbiTrackStart(floorPosition, floorOrientation);
+        }
+	}
+
+
+}
